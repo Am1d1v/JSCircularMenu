a81034cc7f97d244b2d877e9038692b6ff71cce8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-30 16:30:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const container = document.querySelector('.container');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+    container.classList.toggle('active');
+});
